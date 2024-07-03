1b399d292e10f22b7dbf24de282eea0767144490
ssbmars <46971717+ssbmars@users.noreply.github.com>
2024-07-02 21:44:13 -0700

    [WIP] Separate lockout for shield and chips
    Still need to find a safe address to store the shield lockout value. The current address is unvetted

--- bn2sc.asm
+++ bn2sc.asm
@@ -6,12 +6,58 @@
 // the byte at 02008AD0 tracks the duration of chip lockout
 // cl can be applied simply by writing to this address
 
+ShieldOffset EQU 0x19
+
+// hook shield code to run new function at the end
 .org 0x08087534
 	bl	ShieldLag
 
-.org 0x0806CD30
+// track shield lockout separately from chip lockout
+.org 0x08089216
+	bl	ReadChipLockout
+	mov		r1,0x10
+	tst		r6,r1
+	bne		AllowChip
+	bl		ReadAbilityLockout
+	b		AllowAbility
+	nop
+	
+.org 0x08089228	::	AllowChip:
+.org 0x08089230	::	AllowAbility:
+
+
 // write new code here
 // this is "free space" but I'm not entirely sure it's unused 
+.org 0x0806CD30
+
+
+ReadChipLockout:
+	// also iterate ability lockout since this part always runs
+	ldrb	r0,[r5,ShieldOffset]
+	sub		r0,0x1
+	bmi		@@skipwrite
+	strb	r0,[r5,ShieldOffset]
+	// the real chip part
+	@@skipwrite:
+	mov		r3,0x60
+	ldrb	r0,[r5,r3]
+	sub		r0,0x1
+	bmi		@@continue
+	strb	r0,[r5,r3]
+	pop		r15
+	@@continue:
+	mov		r15,r14
+
+
+ReadAbilityLockout:
+	ldrb	r0,[r5,ShieldOffset]
+	sub		r0,0x1
+	bmi		@@continue
+	pop		r15
+	@@continue:
+	mov		r15,r14
+
+
 
 ShieldLag:
 mov		r0,0x8
@@ -23,11 +69,11 @@ ldrb	r0,[r5,r0]
 cmp		r0,2		//this is the level indicator for the shield routine
 bne		@@exit
 //	apply cooldown
-mov		r1,0x60
+mov		r1,0x19		//offset for shield cooldown
 ldrb	r0,[r5,r1]
-cmp		r0,30
+cmp		r0,40
 bgt		@@exit
-mov		r0,30
+mov		r0,40
 strb	r0,[r5,r1]
 @@exit:
 mov		r15,r14
